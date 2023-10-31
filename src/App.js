6d0f23832b4e88c634762a53985aefae47957be3
Won A Young <zeronediary@naver.com>
2023-10-31 16:48:20 +0900

    스타일 적용 연습 flexbox

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
 function App() {
-  return <div></div>;
+  return (
+    <div style={{ display: "flex", justifyContent: "space-evenly" }}>
+      <MyComp />
+      <MyComp />
+      <MyComp />
+    </div>
+  );
 }
 
+function MyComp() {
+  const name = "흥민";
+  return (
+    <div
+      style={{
+        backgroundColor: "blue",
+        color: "white",
+        padding: "10px",
+        width: "25%",
+        textAlign: "center",
+      }}
+    >
+      {name}
+    </div>
+  );
+}
 export default App;
