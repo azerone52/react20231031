efe3911d73dbf4d1b829429c070ce94b424141fb
Won A Young <zeronediary@naver.com>
2023-10-31 14:25:32 +0900

    종료태그 작성연습 완료, 컴포넌트 여러번 사용 연습 완료

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <MyComponent1 />
+      <MyComponent1 />
+      <MyComponent1 />
+    </div>
+  );
 }
-
+function MyComponent1() {
+  return (
+    <div style={{ width: "100%" }}>
+      <img src="https://i.imgur.com/YNT5IoO_d.webp?maxwidth=520&shape=thumb&fidelity=high" />
+    </div>
+  );
+}
 export default App;
