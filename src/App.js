313be4077986ed1f9508e2e8a579ef7261a63427
Won A Young <zeronediary@naver.com>
2023-10-31 14:54:21 +0900

    jsx에서 js 코드 작성은 {} 사용+난수발생

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ function MyComp() {
   const country = "korea";
   const phone = "0109999999";
   const age = 44;
+  const randomNumber = Math.random();
   return (
     <>
       <h1>age는 {age}</h1>
       <h1>address는 {address}</h1>
       <h1>country는 {country}</h1>
       <h1>전화번호는 {phone}</h1>
+      <h1>난수 : {randomNumber}</h1>
+      <h1>1~10 사이 난수: {Math.ceil(randomNumber * 10)}</h1>
     </>
   );
 }
