648fcff9d6aed0a8be839721056a7a0e7b47ed50
Won A Young <zeronediary@naver.com>
2023-10-31 11:40:12 +0900

    component, 주석, jsx

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>hello react</h1>
+        <h2>Hi React</h2>
+        <MyComp />
     </div>
   );
+}
+
+// built-in component는 소문자로 시작(ex. div)
+// component 명은 꼭 대문자로 시작
+// return 문을 가져야 함
+// return 값은 보통 JSX 코드
+// root tag가 필요없다면 fragment 사용 가능
+
+function MyComp(){
+    // return에 jsx 코드 작성 가능
+    // 꼭 하나의 root tag가 존재해야 함
+    // return 값이 여러 줄이면 ()로 감싸기
+
+    // jsx에서 주석 사용법
+    // {/*   */}
+    return (
+        <>
+            {/* fragment는 <></> */}
+            <h1>Hi Component</h1>
+            <h2>hello</h2>
+        </>
+    )
 }
 
 export default App;
