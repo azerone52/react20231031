74518a492c8c6fd3b8d3f9be4df2c03f93f6f58c
Won A Young <zeronediary@naver.com>
2023-10-31 14:49:43 +0900

    jsx에서 js코드 작성하기 연습

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+function MyComp() {
+  const address = "seoul";
+  const country = "korea";
+  const phone = "0109999999";
+  const age = 44;
+  return (
+    <>
+      <h1>age는 {age}</h1>
+      <h1>address는 {address}</h1>
+      <h1>country는 {country}</h1>
+      <h1>전화번호는 {phone}</h1>
+    </>
+  );
+}
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <MyComp />
+    </div>
+  );
 }
 
 export default App;
