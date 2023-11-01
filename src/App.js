dc0d7fe3920000f002211ff1b2994b6cfebc6671
Won A Young <zeronediary@naver.com>
2023-11-01 10:46:16 +0900

    props 구조분해 할당 연습

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <MyElem name={"흥민"} age={44} team={"토트넘"} />
+      <MyElem name={"강인"} age={44} team={"파리"} />
+    </div>
+  );
 }
-
+function MyElem({ name, age, team }) {
+  return (
+    <div>
+      <h1>이름: {name}</h1>
+      <h2>나이: {age}</h2>
+      <h2>팀: {team}</h2>
+      <br />
+    </div>
+  );
+}
 export default App;
