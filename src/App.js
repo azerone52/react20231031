ddd2379a777ec83282eead21d528c4811b6f32bb
Won A Young <zeronediary@naver.com>
2023-11-01 11:33:42 +0900

    props 구조분해 할당, 기본값주기 연습

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <MyComp m="30px" p="10px" color="gold" bg="black" text="hello react" />
+      <MyComp m="30px" p="10px" color="gold" bg="black" />
+      <MyComp m="30px" bg="black" text="hello react" />
+    </div>
+  );
 }
-
+function MyComp({
+  m = "10px",
+  p = "5px",
+  color = "red",
+  bg = "green",
+  text = "default",
+}) {
+  return (
+    <div
+      style={{
+        margin: m,
+        padding: p,
+        color: color,
+        backgroundColor: bg,
+      }}
+    >
+      <h1>{text}</h1>
+    </div>
+  );
+}
 export default App;
