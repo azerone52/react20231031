d6610a85c43c68a87878e6d8fe426e2379312258
Won A Young <zeronediary@naver.com>
2023-11-01 12:22:06 +0900

    한 파일에서 여러개의 값(객체, 값, 함수) export 가능, import 가능

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-// export된 컴포넌트(변수, 함수, 객체)를 import 해서 사용
-import { MyElem } from "./components/MyElem";
-import { MyComp } from "./components/MyComp";
+import {
+  MyBox,
+  MyContainer,
+  MyElem,
+  name,
+  address,
+  person,
+} from "./components/MyBox";
 
 function App() {
+  console.log("name", name);
+  console.log("address", address);
+  console.log("person", person);
   return (
     <>
-      <MyComp />
+      <MyBox />
       <MyElem />
+      <MyContainer />
     </>
   );
 }
---
+++ src/components/MyBox.js
@@ -0,0 +1,13 @@
+export function MyBox() {
+  return null;
+}
+
+export function MyElem() {
+  return null;
+}
+
+export function MyContainer() {}
+
+export let name = "손흥민";
+export const address = "신촌";
+export const person = { name: "강인", team: "파리" };
