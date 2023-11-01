f9e83e4eb7a34f2a6d3739bf3732db22899deac5
Won A Young <zeronediary@naver.com>
2023-11-01 11:13:06 +0900

    props 구조분해 할당 연습

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
 function App() {
-  return <div></div>;
+  return (
+    <>
+      <MyBox
+        p={"10px"}
+        m={"10%"}
+        color={"orange"}
+        text={"2023-11-01"}
+        bg={"blue"}
+      />
+      <MyBox
+        p={"20px"}
+        m={"20%"}
+        color={"pink"}
+        text={"벌써 11월"}
+        bg={"skyblue"}
+      />
+    </>
+  );
 }
 
+function MyBox({ p, m, color, text, bg }) {
+  return (
+    <div style={{ padding: p, margin: m, color: color, backgroundColor: bg }}>
+      <p>{text}</p>
+    </div>
+  );
+}
 export default App;
