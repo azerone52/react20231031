9547fec123959590fe93f021eeff1ab7296b9885
Won A Young <zeronediary@naver.com>
2023-11-01 14:27:24 +0900

    default export, import 연습

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import MyBox, { person, team } from "./component/MyContainer";
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <MyBox />
+      <h1>{person.name}</h1>
+      <h1>{person.address}</h1>
+      <h1>{team.title}</h1>
+      <h1>{team.score}</h1>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/component/MyContainer.js
@@ -0,0 +1,8 @@
+function MyBox() {
+  return <h1>HERE IS MY BOX</h1>;
+}
+
+export const person = { name: "흥민", address: "런던" };
+export const team = { title: "토트넘", score: 100 };
+
+export default MyBox;
