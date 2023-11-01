6ab886595cb5e65391bba78912ca5edecde0881d
Won A Young <zeronediary@naver.com>
2023-11-01 11:22:21 +0900

    props 구조분해 할당, 기본값주기

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 function App() {
   return (
     <>
-      <MyComp value={"흥민"} address="신촌" />
+      <MyComp name={"흥민"} address="신촌" age={33} city="서울" />
+      <MyComp address="강남" age={22} city="부산" />
+      <MyComp address="은행동" city="대전" />
     </>
   );
 }
 
-// {value: "흥민", address: "신촌"}
-function MyComp({ value, address }) {
+function MyComp({ name = "anonymous", address, city, age = 100 }) {
   return (
     <div>
-      <h1>
-        {value}는 {address}에 산다
-      </h1>
+      <p>
+        {name}은 {age}세 이고 {address}, {city}에 산다
+      </p>
     </div>
   );
 }
