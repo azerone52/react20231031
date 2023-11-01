79f65a6781fc334b46be2ea86d7e22e538dd516a
Won A Young <zeronediary@naver.com>
2023-11-01 12:34:20 +0900

    한 파일에서 여러개의 값(객체, 값, 함수) export 가능, import 가능 연습

---
+++ src/MyElem.js
@@ -0,0 +1,14 @@
+export function MyElem() {
+  return null;
+}
+export function MyNavBar() {
+  return null;
+}
+
+export function MyContent() {
+  return null;
+}
+
+export const country = "korea";
+export const text = "home";
+export const value = 52;
