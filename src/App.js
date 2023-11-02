ad0ce0a49f4ffa754da176f391d6bca57ab17a49
Won A Young <zeronediary@naver.com>
2023-11-02 10:23:33 +0900

    필요없는 코드 삭제함

--- src/App.js
+++ src/App.js
@@ -1,23 +1,7 @@
-import { Box } from "@chakra-ui/react";
-
 function App() {
   return (
     <>
-      <Box bgColor={"tomato"}>Lorem ipsum dolor.</Box>
-      <Box bgColor={"red"}>Lorem ipsum dolor.</Box>
-      <Box bgColor={"blue"}>Cum, ipsa sapiente!</Box>
-      <Box bgColor={"blue.50"}>Fugiat, minima necessitatibus.</Box>
-      <Box bgColor={"blue.100"}>Explicabo, necessitatibus, totam!</Box>
-      <Box bgColor={"blue.900"}>Ad, provident, sunt.</Box>
-      <Box bgColor={"gray"}>Aperiam, debitis voluptatibus.</Box>
-      <Box bgColor={"gray.100"}>Officiis, suscipit voluptatibus.</Box>
-      <Box bgColor={"gray.800"} color={"white"}>
-        Assumenda, sint, velit.
-      </Box>
-      <Box bg={"yellow.100"}>Cum, facilis, magnam!</Box>
-      <Box bg={"facebook.500"} color={"linkedin.100"}>
-        Similique, sit, soluta.
-      </Box>
+      <div></div>
     </>
   );
 }
