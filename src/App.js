950635fa5f2a80f552c4dee47eec750a77ac9f49
Won A Young <zeronediary@naver.com>
2023-11-02 14:34:18 +0900

    Button 사용

--- src/App.js
+++ src/App.js
@@ -1,37 +1,32 @@
-import { Button, useToast } from "@chakra-ui/react";
+import { Button } from "@chakra-ui/react";
+import { ArrowRightIcon, EmailIcon } from "@chakra-ui/icons";
 
 function App() {
-  const toast = useToast();
-
   return (
     <>
-      <Button
-        onClick={() =>
-          toast({
-            title: "저장완료",
-            description: "게시물이 저장되었습니다.",
-            duration: 2000,
-            position: "top",
-            isClosable: true,
-          })
-        }
-      >
-        토스트 띄우기
+      <Button colorScheme="blue">Lorem.</Button>
+      <Button colorScheme="red">Quasi.</Button>
+      <Button colorScheme="yellow">Exercitationem.</Button>
+      <Button colorScheme="orange">Eligendi?</Button>
+      <Button bgColor={"orange.300"}>Temporibus.</Button>
+      <Button colorScheme="teal" size={"sm"}>
+        Modi.
       </Button>
-
-      <Button
-        onClick={() =>
-          toast({
-            title: "저장실패",
-            description: "게시물이 저장되지 않았습니다.",
-            duration: 2000,
-            position: "top",
-            isClosable: true,
-            status: "error",
-          })
-        }
-      >
-        토스트 띄우기
+      <Button colorScheme="facebook" size={"lg"}>
+        Vel?
+      </Button>
+      <Button colorScheme="telegram" variant={"outline"}>
+        Debitis!
+      </Button>
+      <Button colorScheme="orange" variant={"ghost"}>
+        Molestiae.
+      </Button>
+      <Button variant={"link"}>Quam?</Button>
+      <Button leftIcon={<EmailIcon />}>Similique.</Button>
+      <Button rightIcon={<ArrowRightIcon />}>Quidem.</Button>
+      <Button isLoading>Commodi!</Button>
+      <Button isLoading loadingText={"전송중"}>
+        Ab!
       </Button>
     </>
   );
