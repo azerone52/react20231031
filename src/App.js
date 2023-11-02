9ad1a3261e2de9e223e461b8c147fbbe9f4b9e8c
Won A Young <zeronediary@naver.com>
2023-11-02 15:20:34 +0900

    FormControl-Textarea 사용

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 import React from "react";
-import { Box, FormControl, Select } from "@chakra-ui/react";
+import { Box, FormControl, FormLabel, Textarea } from "@chakra-ui/react";
 
 function App(props) {
   return (
     <div>
       <Box w={"480px"}>
         <FormControl>
-          <Select placeholder="선택하세요." multiple h={"120px"}>
-            <option value="value1">Lorem.</option>
-            <option value="value2">Sed.</option>
-            <option value="value3">Fuga.</option>
-            <option value="value4">Aliquid?</option>
-            <option value="value5">Voluptate!</option>
-          </Select>
+          <FormLabel>본문</FormLabel>
+          <Textarea placeholder="본문을 작성하세요" h={"500px"}></Textarea>
         </FormControl>
       </Box>
     </div>
