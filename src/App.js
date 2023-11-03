2a7b6c8baa2982dd142e4135c66ba6df77f4b6f4
Won A Young <zeronediary@naver.com>
2023-11-03 10:22:04 +0900

    함수를 props으로 전달

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
-function MyComp({ text, address, children }) {
+import { Button } from "@chakra-ui/react";
+
+function MyComp({ to, someProp }) {
+  someProp();
   return (
     <>
-      <p>{text}</p>
-      <p>{address}</p>
-      <p>{children}</p>
+      <div>
+        <a href={to.link}>link!!</a>
+      </div>
+      <div>
+        <Button onClick={someProp}>버튼</Button>
+      </div>
     </>
   );
 }
 
 function App(props) {
+  function func1() {
+    console.log("func1 실행됨");
+  }
   return (
     <div>
-      <MyComp text={"hello"} address={"seoul"}>
-        some contents 다른 컴포넌트가 있을 수도 있음
-      </MyComp>
+      <MyComp to={{ link: "https://www.naver.com" }} someProp={func1}></MyComp>
     </div>
   );
 }
