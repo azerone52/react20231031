37cd35180c8a26a6f37a6b0c6944c5d6e0c4b449
Won A Young <zeronediary@naver.com>
2023-11-03 12:33:28 +0900

    event.preventDefault(): 기본 기능 취소 연습

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 function App() {
+  function handleClick(e) {
+    //a 태그 클릭시 네이버로 이동하도록
+    e.preventDefault();
+    // console.log("테스트");
+    window.location.href = "https://www.naver.com";
+  }
   return (
     <>
-      <div></div>
+      <a href="http://www.daum.net" onClick={handleClick}>
+        다음으로 이동
+      </a>
     </>
   );
 }
