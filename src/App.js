e51aa1b7cd17b8eeba03927265adf0843fc88150
Won A Young <zeronediary@naver.com>
2023-11-03 10:54:50 +0900

    파라미터 값이 있는 함수를 이벤트에 사용하기

--- src/App.js
+++ src/App.js
@@ -1,31 +1,20 @@
 import { Button } from "@chakra-ui/react";
 
-function MyComp({ children, executeClick }) {
-  return <Button onClick={executeClick}>{children}</Button>;
-}
-
 function App(props) {
-  function func1() {
-    console.log("func1 실행");
+  function handleClickButton1() {
+    console.log("1번 버튼 클릭됨");
   }
-  let func2 = () => {
-    console.log("arrow function 실행 11");
-  };
+  function handleClickButton2() {
+    console.log("2번 버튼 클릭됨");
+  }
+  function handleButtonClick(number) {
+    console.log(number + "번째 버튼 클릭됨");
+  }
   return (
     <div>
-      <MyComp executeClick={func1}>Button1</MyComp>
-      <MyComp executeClick={func2}>Button2</MyComp>
-      <MyComp
-        executeClick={() => {
-          console.log("arrow function 실행 22");
-        }}
-      >
-        Button3
-      </MyComp>
-      {/*중괄호 없는 arrow function=> 실행문이 바로 실행되고 그게 리턴임*/}
-      <MyComp executeClick={() => console.log("arrow function 실행 333")}>
-        Button4
-      </MyComp>
+      <Button onClick={handleClickButton1}>첫번째 버튼</Button>
+      <Button onClick={handleClickButton2}>두번째 버튼</Button>
+      <Button onClick={() => handleButtonClick(3)}>세번째 버튼</Button>
     </div>
   );
 }
