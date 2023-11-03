04a4ab2a57dcb5c157028f1bbd2b6b28abf545c6
Won A Young <zeronediary@naver.com>
2023-11-03 10:48:50 +0900

    함수를 props으로 전달하는 다양한 방법(arrow function) 연습

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import { Button } from "@chakra-ui/react";
+
 function App() {
   return (
     <>
-      <div></div>
+      {/*첫번째 버튼 클릭시 first 콘솔에 출력 두번째 버튼은 second*/}
+      <Button onClick={() => console.log("first")}>Lorem.</Button>
+      <Button onClick={() => console.log("second")}>ipsum.</Button>
     </>
   );
 }
