705682038ec5ff7340e954e73c50e8ed826887a6
Won A Young <zeronediary@naver.com>
2023-11-03 15:18:25 +0900

    예제)text input으로 상태변경

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
 import React, { useState } from "react";
-import { Button, Text } from "@chakra-ui/react";
+import { Input, Text } from "@chakra-ui/react";
 
 function App(props) {
-  const [number, setNumber] = useState(0);
-  // let number = 0;
-  function handleClick() {
-    // number++;
-    // document.querySelector(".txt").value = number;
-    //리액트가 클래스명 등을 관리하기 때문에 유효하지 않음
-    setNumber(number + 1);
+  const [message, setMessage] = useState("");
+  function handleInputChange(e) {
+    setMessage(e.target.value);
   }
   return (
     <div>
-      <Button onClick={handleClick}>바꾸기</Button>
-      <Text className="txt">{number}</Text>
+      <Input onChange={handleInputChange} />
+      <Text>{message}</Text>
     </div>
   );
 }
