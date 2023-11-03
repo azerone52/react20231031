ceb636797c283c8b5bb76afff49cb08b28727a0a
Won A Young <zeronediary@naver.com>
2023-11-03 15:12:00 +0900

    useState() 상태를 변경할 때 set메소드 사용해야 함

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App(props) {
     // number++;
     // document.querySelector(".txt").value = number;
     //리액트가 클래스명 등을 관리하기 때문에 유효하지 않음
-    setNumber(number);
+    setNumber(number + 1);
   }
   return (
     <div>
