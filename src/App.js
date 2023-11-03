4af18407ae3eed2fdd224e4aaaef0417202af41b
Won A Young <zeronediary@naver.com>
2023-11-03 09:35:44 +0900

    button link

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
+import { Button, Flex } from "@chakra-ui/react";
+
+function MyLink({ to, children }) {
+  return (
+    <>
+      <Flex>
+        <Button m="10px" variant={"link"}>
+          <a href={to}>{children}</a>
+        </Button>
+        <Button as={"a"} href={to}>
+          {children}
+        </Button>
+      </Flex>
+    </>
+  );
+}
+
 function App() {
   return (
     <>
-      <div></div>
+      <MyLink to={"https://www.naver.com"}>naver로</MyLink>
+      <MyLink to={"https://www.daum.net"}>daum으로</MyLink>
     </>
   );
 }
