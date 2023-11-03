738fd3dfe0c8562d21d1d8626486bbde82d664f8
Won A Young <zeronediary@naver.com>
2023-11-03 14:57:06 +0900

    useState() 상태를 변경할 때 set메소드 사용해야 함

--- src/App.js
+++ src/App.js
@@ -1,36 +1,19 @@
 import React, { useState } from "react";
-import { Button, Input } from "@chakra-ui/react";
+import { Button, Text } from "@chakra-ui/react";
 
 function App(props) {
-  // 상태(state)가 변경되면(이전 state와 달라짐)되면
-  // Component를 다시 그림(re render)
-
-  // state: react가 관리하는 component의 값
-
-  // state를 얻는 방법
-  // useState();
-
-  //배열을 리턴함
-  //index 0: 현재상태(state)
-  //index 1: state를 변경할 수 있는 메소드
-  const [state, func] = useState("");
-  // const [message, setMessage] = useState("");
-  //실제로 상태가 바뀔 때에만  컴포넌트가 re render 됨
-  let text = "초기 메시지";
-  function handleInputChange(e) {
-    text = e.taget.value;
-    console.log("text", text);
-  }
-  function handleButtonClick(e) {
-    func("바뀐 상태");
+  const [number, setNumber] = useState(0);
+  // let number = 0;
+  function handleClick() {
+    // number++;
+    // document.querySelector(".txt").value = number;
+    //리액트가 클래스명 등을 관리하기 때문에 유효하지 않음
+    setNumber(number);
   }
   return (
     <div>
-      <Input onChange={handleInputChange} />
-      <p>{text}</p>
-      <Button onClick={handleButtonClick}>상태 바꾸기</Button>
-      <Button onClick={() => func("")}>빈 문자열로 바꾸기</Button>
-      <p>{state}</p>
+      <Button onClick={handleClick}>바꾸기</Button>
+      <Text className="txt">{number}</Text>
     </div>
   );
 }
