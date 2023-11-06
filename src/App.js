996bbf90ead3a05c7a45966796067c7cb567ed15
Won A Young <zeronediary@naver.com>
2023-11-06 14:22:21 +0900

    배열을 상태로 쓸 경우 상태 변경 연습

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
+import { useState } from "react";
+import { Box, Button, ListItem, OrderedList } from "@chakra-ui/react";
+
 function App() {
+  const [items, setItems] = useState([]);
+
+  function handleButtonClick(item) {
+    console.log(item);
+
+    // const newItems = [...items];
+    // newItems.push(item);
+
+    // const newItems = [...items, item];
+    // setItems(newItems);
+    setItems([...items, item]);
+  }
+
   return (
     <>
-      <div></div>
+      <Button onClick={() => handleButtonClick("커피")}>커피</Button>
+      <Button onClick={() => handleButtonClick("케이크")}>케이크</Button>
+      <Button onClick={() => handleButtonClick("아이스크림")}>
+        아이스크림
+      </Button>
+      <Button onClick={() => handleButtonClick("크로아상")}>크로아상</Button>
+      <Button onClick={() => handleButtonClick("단팥빵")}>단팥빵</Button>
+
+      <Box>
+        <OrderedList>
+          {items.map((item, index) => (
+            <ListItem key={index}>{item}</ListItem>
+          ))}
+        </OrderedList>
+      </Box>
     </>
   );
 }
