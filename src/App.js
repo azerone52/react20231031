9502ccf4c5c3d75d8839a3d71b428f6e07d59b8e
Won A Young <zeronediary@naver.com>
2023-11-06 11:38:13 +0900

    객체는 복사해서 새 객체를 state로 변경 연습

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
+import { Box, Input, Text } from "@chakra-ui/react";
+import { useState } from "react";
+
 function App() {
+  const [message, setMessage] = useState("");
+  const [obj, setObj] = useState({ message: "" });
+
+  function handleObjectMessageChange(e) {
+    // const newObj = { ...obj };
+    // newObj.message = e.target.value;
+    //
+    // setObj(newObj);
+
+    setObj({ ...obj, message: e.target.value });
+  }
+
   return (
     <>
-      <div></div>
+      <Box>
+        <Input value={message} onChange={(e) => setMessage(e.target.value)} />
+        <Text>{message}</Text>
+      </Box>
+      <Box>
+        <Input value={obj.message} onChange={handleObjectMessageChange} />
+        <Text>{obj.message}</Text>
+      </Box>
     </>
   );
 }
