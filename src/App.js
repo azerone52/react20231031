f957ebed79fccbf8f0b0c337cd843af43123e953
Won A Young <zeronediary@naver.com>
2023-11-07 16:43:52 +0900

    ajax get 사용 state 업데이트 스프링부트 이용 연습 여러 property 가져와 보기

--- src/App.js
+++ src/App.js
@@ -2,18 +2,25 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 function App(props) {
-  const [message, setMessage] = useState("");
+  const [customer, setCustomer] = useState({});
   useEffect(() => {
     axios
-      .get("/api/main1/sub2")
+      .get("/api/main1/sub3")
       .then((response) => response.data)
-      .then((data) => setMessage(data))
+      .then((data) => setCustomer(data))
       .catch((err) => console.log(err));
   }, []);
   // useEffect로
-  // get 요청 /api/main1/sub2
+  // get 요청 /api/main1/sub3
   // 받은 값으로 message state를 업데이트
-  return <div>{message}</div>;
+  return (
+    <div>
+      <h1>고객 정보</h1>
+      <h1>{customer.customerId}</h1>
+      <h1>{customer.customerName}</h1>
+      <h1>{customer.address}</h1>
+    </div>
+  );
 }
 
 export default App;
