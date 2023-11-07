2db2ad1e16c3b4fcaf0eef7c84c89c61ec4a8be1
Won A Young <zeronediary@naver.com>
2023-11-07 10:19:37 +0900

    다른 컴포넌트의 상태 바꾸기: 공통 부모에게 상태를 두고 사용해야 함 ->app안에 setMessage 작동 함수 만들고 그걸 전달하는 버전

--- src/App.js
+++ src/App.js
@@ -12,19 +12,24 @@ function MyComp2({ message }) {
 function MyComp1({ onClick }) {
   return (
     <Box>
-      <Button onClick={() => onClick("world")}>텍스트 바꾸기!</Button>
+      <Button onClick={onClick}>텍스트 바꾸기!</Button>
     </Box>
   );
 }
 
 function App(props) {
   const [message, setMessage] = useState("hello");
+
+  function handleButtonClick() {
+    setMessage("안녕하세요");
+  }
+
   return (
     <div>
-      <MyComp1 onClick={setMessage}></MyComp1>
-      {/* MyComp1에 있는 버튼이 클릭되면 */}
+      <MyComp1 onClick={handleButtonClick}></MyComp1>
+      {/* MyComp1 에 있는 버튼이 클릭되면 */}
       <MyComp2 message={message}></MyComp2>
-      {/* MyComp2에 있는 텍스트가 바뀌도록 */}
+      {/*  MyCom2에 있는 text가 바뀌게 */}
     </div>
   );
 }
